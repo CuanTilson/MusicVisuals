7a54cdbbd26876b3c6ee9be23291c7e8b39264ea
Sagar-TUD-student <sagar3013singh@gmail.com>
2024-03-24 17:59:04 +0000

    Update musicVisualiserProject.java

--- java/src/ie/tudublin/musicVisualiserProject.java
+++ java/src/ie/tudublin/musicVisualiserProject.java
@@ -1,5 +1,21 @@
 package ie.tudublin;
 
+import example.AudioBandsVisual;
+import example.WaveForm;
+
 public class musicVisualiserProject extends Visual {
 
+    public void settings() {
+        size(1024, 500);
+    }
+
+    public void keyPressed() {
+        if (key == ' ') {
+        }
+    }
+
+    public void draw() {
+        background(0);
+
+    }
 }
