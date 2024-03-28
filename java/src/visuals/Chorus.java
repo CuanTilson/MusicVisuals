91ae715ead8594e80252bef594183885648bf10d
Cúán Tilson <c22385233@mytudublin.ie>
2024-03-28 13:16:34 +0000

    Adding some sample code to chorus.java

--- java/src/visuals/Chorus.java
+++ java/src/visuals/Chorus.java
@@ -1,5 +1,24 @@
 package visuals;
 
+import processing.core.*;
+
 public class Chorus {
 
+    // Reference to the main MusicVisualiserProject object
+    MusicVisualiserProject mvp;
+
+    // Constructor that accepts a MusicVisualiserProject object
+    public Chorus(MusicVisualiserProject mvProject) {
+        this.mvp = mvProject;
+    }
+
+    // Method to render the Chorus visual
+    public void render() {
+        float gap = mvp.width / (float) mvp.getBands().length;
+        mvp.noStroke();
+        for (int i = 0; i < mvp.getBands().length; i++) {
+            mvp.fill(PApplet.map(i, 0, mvp.getBands().length, 255, 0), 255, 255);
+            mvp.rect(i * gap, mvp.height, gap, -mvp.getSmoothedBands()[i] * 10f);
+        }
+    }
 }
