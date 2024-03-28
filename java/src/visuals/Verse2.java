c131f698f3d02155fc9d1a74e11de4e58c0653be
Cúán Tilson <c22385233@mytudublin.ie>
2024-03-28 16:42:53 +0000

    Added code to run MusicVisualiserProject.java

--- java/src/visuals/Verse2.java
+++ java/src/visuals/Verse2.java
@@ -1,5 +1,23 @@
 package visuals;
 
+import processing.core.*;
+
 public class Verse2 {
 
+    // Reference to the main MusicVisualiserProject object
+    MusicVisualiserProject mvp;
+
+    // Constructor that accepts a MusicVisualiserProject object
+    public Verse2(MusicVisualiserProject mvProject) {
+        this.mvp = mvProject;
+    }
+
+    // Method to render the Chorus visual
+    public void render() {
+        // Text saying Chorus centred on screen
+        mvp.textSize(32);
+        mvp.textAlign(PConstants.CENTER, PConstants.CENTER);
+        mvp.fill(255);
+        mvp.text("Verse 2", mvp.width / 2, mvp.height / 2);
+    }
 }
