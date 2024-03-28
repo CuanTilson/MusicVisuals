0e099ac1f25e5022c9fb8691909d57582b4eda0c
Cúán Tilson <c22385233@mytudublin.ie>
2024-03-28 16:42:45 +0000

    Added code to run MusicVisualiserProject.java

--- java/src/visuals/Prechorus.java
+++ java/src/visuals/Prechorus.java
@@ -1,5 +1,23 @@
 package visuals;
 
+import processing.core.*;
+
 public class Prechorus {
 
+    // Reference to the main MusicVisualiserProject object
+    MusicVisualiserProject mvp;
+
+    // Constructor that accepts a MusicVisualiserProject object
+    public Prechorus(MusicVisualiserProject mvProject) {
+        this.mvp = mvProject;
+    }
+
+    // Method to render the Chorus visual
+    public void render() {
+        // Text saying Chorus centred on screen
+        mvp.textSize(32);
+        mvp.textAlign(PConstants.CENTER, PConstants.CENTER);
+        mvp.fill(255);
+        mvp.text("Prechorus", mvp.width / 2, mvp.height / 2);
+    }
 }
