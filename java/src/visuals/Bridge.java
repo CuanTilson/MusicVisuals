4974ea5155de20131efe88de600c2528363df63a
Hemant Sundarrajan <C22440886@mytudublin.ie>
2024-04-16 11:20:56 +0100

    Added Terrain to visual
    Made Terrain react to sound

--- java/src/visuals/Bridge.java
+++ java/src/visuals/Bridge.java
@@ -4,36 +4,49 @@ import processing.core.*;
 
 public class Bridge extends PApplet
 {
+    MusicVisualiserProject mvp;
     int cols, rows;
-    int scl = 20;
     float[][] terrain;
-    int w = 2048;
-    int h  = 1500;
     float flying;
-    float smoothedAmplitude = 0;
+    float amplitude;
+    float smoothedAmplitude;
+    int w;
+    int h;
+    int increase;
+    int scl = 20;
 
     // Reference to the main MusicVisualiserProject object
-    MusicVisualiserProject mvp;
 
     // Constructor that accepts a MusicVisualiserProject object
-    public Bridge(MusicVisualiserProject mvProject) {
-        this.mvp = mvProject;
+    public Bridge(MusicVisualiserProject mvp) {
+        this.mvp = mvp;
+        increase = 1000;
+
+
+        
     }
 
     // Method to render the Chorus visual
     public void render() {
+        w = mvp.width + increase;
+        h = mvp.height + increase;
+        cols = w / scl;
+        rows = h / scl;
+        terrain = new float[cols][rows];
         // Text saying Chorus centred on screen
-        mvp.textSize(32);
-        mvp.textAlign(PConstants.CENTER, PConstants.CENTER);
-        mvp.fill(255);
-        mvp.text("Bridge", mvp.width / 2, mvp.height / 2);
+        // mvp.textSize(32);
+        // mvp.textAlign(PConstants.CENTER, PConstants.CENTER);
+        // mvp.fill(255);
+        // mvp.text("Bridge", mvp.width / 2, mvp.height / 2);
+        drawTerrain();
     }
 
     public void drawTerrain()
     {
         flying  -=  0.05;
 
-        float amplitude = smoothedAmplitude * 6000;
+        smoothedAmplitude = mvp.getSmoothedAmplitude();
+        amplitude = smoothedAmplitude * 6000;
 
         float yoff = flying;
         for (int y = 0;  y  < rows; y++){
@@ -46,23 +59,23 @@ public class Bridge extends PApplet
         }
 
         
-        background(0);
-        stroke(255);
-        noFill();
+        mvp.background(0);
+        mvp.stroke(255);
+        mvp.noFill();
         
-        translate(width/2, height/2);
-        rotateX(PI/3);
+        mvp.translate(w/2, h/2);
+        mvp.rotateX(PI/3);
 
-        translate(-w/2, -h/2);
+        mvp.translate(-w/2-increase/2, -h/2);
         for (int y = 0;  y  < rows-1; y++){
-            beginShape(TRIANGLE_STRIP);
+            mvp.beginShape(TRIANGLE_STRIP);
             for (int x = 0;  x < cols; x++){
                 //rect(x *scl, y*scl, scl,scl);
-                vertex(x*scl, y*scl, terrain[x][y]);
-                vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+                mvp.vertex(x*scl, y*scl, terrain[x][y]);
+                mvp.vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
 
             }
-            endShape();
+            mvp.endShape();
         }
     }
 }
