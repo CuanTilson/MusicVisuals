03596f5b509ea2c6e029d490b153fbbc5b0c77b7
Hemant Sundarrajan <C22440886@mytudublin.ie>
2024-04-16 07:51:09 +0100

    Added most of Intial terrain code

--- java/src/visuals/Bridge.java
+++ java/src/visuals/Bridge.java
@@ -10,6 +10,8 @@ public class Bridge extends PApplet
     int w = 2048;
     int h  = 1500;
     float flying;
+    float smoothedAmplitude = 0;
+
     // Reference to the main MusicVisualiserProject object
     MusicVisualiserProject mvp;
 
@@ -25,5 +27,42 @@ public class Bridge extends PApplet
         mvp.textAlign(PConstants.CENTER, PConstants.CENTER);
         mvp.fill(255);
         mvp.text("Bridge", mvp.width / 2, mvp.height / 2);
+    }
+
+    public void drawTerrain()
+    {
+        flying  -=  0.05;
+
+        float amplitude = smoothedAmplitude * 6000;
+
+        float yoff = flying;
+        for (int y = 0;  y  < rows; y++){
+            float xoff  = 0;
+            for (int x = 0;  x < cols; x++){
+                terrain[x][y] = map(noise(xoff, yoff) , 0, 1, -amplitude, amplitude);
+                xoff += 0.1;
+            }
+            yoff +=  0.1;
+        }
+
+        
+        background(0);
+        stroke(255);
+        noFill();
+        
+        translate(width/2, height/2);
+        rotateX(PI/3);
+
+        translate(-w/2, -h/2);
+        for (int y = 0;  y  < rows-1; y++){
+            beginShape(TRIANGLE_STRIP);
+            for (int x = 0;  x < cols; x++){
+                //rect(x *scl, y*scl, scl,scl);
+                vertex(x*scl, y*scl, terrain[x][y]);
+                vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+
+            }
+            endShape();
+        }
     }
 }
